14d1259098b99c67e4b4ec4e147fde4171545331
Daniel Galeano <daniel.rgo.13@hotmail.com>
2019-12-01 16:45:11 -0500

    edit index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="table-responsive-lg">
-        <h1>Invoice Management System</h1>
-    </div>
+<div class="btn-group btn-group-sm" role="group" aria-label="{{ __('menu') }}">
+    <a href="{{ route('invoice.index') }}" class="btn btn-link" title="{{ __('invoice') }}">
+        <i class="fas fa-eye">Invoice</i>
+    </a>
+    <a href="{{ route('client.index') }}" class="btn btn-link" title="{{ __('client') }}">
+        <i class="fas fa-edit">Client</i>
+    </a>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,13 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+                <a href="{{ route('invoice.index') }}" class="btn btn-link" title="{{ __('invoice') }}">
+                    <i class="fas fa-eye">Invoice</i>
+                </a>
+                <a href="{{ route('client.index') }}" class="btn btn-link" title="{{ __('client') }}">
+                    <i class="fas fa-edit">Client</i>
+                </a>
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
