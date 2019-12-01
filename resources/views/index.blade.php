99953dfc21102e158e2c605f8dc6c3b6e321c179
Daniel Galeano <daniel.rgo.13@hotmail.com>
2019-12-01 15:15:59 -0500

    ajust login and index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="card card-default">
-        <div class="card-header pb-0">
-            <h5 class="card-title">{{ __('Invoices') }}</h5>
-        </div>
-        <div class="card-footer d-flex justify-content-between">
-            <a class="btn btn-primary" href="{{ route('invoice.create') }}">Create new invoice</a>
-        </div>
-    </div>
-
-
     <div class="table-responsive-lg">
         <h1>Invoice Management System</h1>
     </div>
