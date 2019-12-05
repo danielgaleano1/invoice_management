e47f26be03e877ebec701c0fbf1362ba9e13c8b5
Daniel Galeano <daniel.rgo.13@hotmail.com>
2019-12-04 23:50:00 -0500

    edit form to view create and edit of product

--- resources/views/product/__form.blade.php
+++ resources/views/product/__form.blade.php
@@ -7,4 +7,28 @@
             @includeWhen($errors->has('code'), 'partials.__invalid_feedback', ['feedback' => $errors->first('code')])
         </div>
     </div>
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="description">{{ __('Description') }}</label>
+            <input type="text" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="description" value="{{ old('description', $product_list->description) }}" required>
+            @includeWhen($errors->has('description'), 'partials.__invalid_feedback', ['feedback' => $errors->first('description')])
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="stock">{{ __('Stock') }}</label>
+            <input type="number" class="form-control {{ $errors->has('stock') ? 'is-invalid' : '' }}" name="stock" id="stock" value="{{ old('stock', $product_list->stock) }}" required>
+            @includeWhen($errors->has('stock'), 'partials.__invalid_feedback', ['feedback' => $errors->first('stock')])
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="price">{{ __('Price') }}</label>
+            <input type="number" class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" name="price" id="price" value="{{ old('price', $product_list->price) }}" required>
+            @includeWhen($errors->has('price'), 'partials.__invalid_feedback', ['feedback' => $errors->first('price')])
+        </div>
+    </div>
 </div>
