ee997c02de8cc5639ad1dde20ae8a71b11de0fc8
Daniel Galeano <daniel.rgo.13@hotmail.com>
2019-12-09 04:36:53 -0500

    delete rows function search_product

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,14 +2,4 @@ function search_product(){
     var product_id_form=$("#product_id").val();
     var url="";
 
-    $.ajax({
-
-    })
-    
-
-    $.get(url,function(resul){
-        $("#price").val('resul');
-        console.log(1);
-    })
-}
  
