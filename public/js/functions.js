8e34d5ec636740a7aec1a4ff237c4a4ed259df11
Daniel Galeano <daniel.rgo.13@hotmail.com>
2019-12-13 23:51:30 -0500

    deelet rows not necesary in function.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,8 +4,7 @@ function search_product(){
     $.getJSON(
         '/invoice_product/' + product_id_form,
         function(json) { 
-            var price_bd = json.price;
-            $("#price").val(price_bd);
+            $("#price").val(json.price);
             $("#stock").val(json.stock);
         }
       );
