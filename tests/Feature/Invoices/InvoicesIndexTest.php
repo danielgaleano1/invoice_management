c0c49e2dd95e0b21cfc5a7f6804aa1a8135f169d
daniel <daniel.rgo.13@gmail.com>
2020-02-05 00:44:27 -0500

    tests of invoices

--- tests/Feature/Invoices/InvoicesIndexTest.php
+++ tests/Feature/Invoices/InvoicesIndexTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use App\Invoice;
 
 class InvoicesIndexTest extends TestCase
 {
@@ -23,5 +24,35 @@ class InvoicesIndexTest extends TestCase
         $response = $this->actingAs($user)->get(route('invoice.index'));
 
         $response->assertSuccessful();
+        $response->assertViewIs('invoice.index');
+    }
+
+    /** @test */
+
+    public function index_of_invoices_has_content()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get(route('invoice.index'));
+
+        $response->assertSee('Invoices');
+    }
+
+    /** @test */
+    public function invoice_information_displayed_on_index()
+    {
+        $user = factory(User::class)->create();
+        $invoice = factory(Invoice::class)->create();
+
+        $response = $this->actingAs($user)->get(route('invoice.index'));
+
+        $response->assertSee($invoice->code);
+        $response->assertSee($invoice->collaborator_id);
+        $response->assertSee($invoice->invoice_state_id);
+        $response->assertSee($invoice->client_id);
+        //$response->assertSee($invoice->expiration_at);
+        //$response->assertSee($invoice->date_of_receipt);
+        $this->assertIsInt($invoice->value_tax);
+        $this->assertIsInt($invoice->total_value);
+        //$response->assertSee($invoice->date_of_receipt);
     }
 }
